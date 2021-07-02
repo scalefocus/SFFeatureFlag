b4ab1434cd816232f7df77e7c5e25d16341444a2
Maja Sapunova <maja.sapunova@scalefocus.com>
2021-07-02 10:27:43 +0200

    Added Pods to .gitignore

--- Example/Pods/Target Support Files/Pods-SFFeatureFlag_Example/Pods-SFFeatureFlag_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SFFeatureFlag_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SFFeatureFlag_Example
-@end
--- Example/Pods/Target Support Files/SFFeatureFlag/SFFeatureFlag-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SFFeatureFlag : NSObject
-@end
-@implementation PodsDummy_SFFeatureFlag
-@end
